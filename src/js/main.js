31ded2c741088b94096b15b876a7b1c341c5d79d
Maruf Mia <mmaruf1137@gmail.com>
2022-09-05 22:52:48 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 window.addEventListener('scroll', () => {
 	const nav = document.querySelector('.nav');
+	const btnScrollToTop = document.querySelector('.btn-scrollToTop');
 	const target = window.scrollY;
 	if (target > 100) {
 		nav.classList.add('sticky');
+		btnScrollToTop.classList.add('show-btn');
 	} else {
 		nav.classList.remove('sticky');
+		btnScrollToTop.classList.remove('show-btn');
 	}
 });
